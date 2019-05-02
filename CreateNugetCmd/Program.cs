7caebbb9e1f93d2b8d1c714204a8fcd5337583ac
Bob Goodearl <bg.gitguy@rgoodsoftware.com>
2019-05-02 17:50:27 -0400

    Console app to create .cmd file to register local NuGet  pkg

--- CreateNugetCmd/Program.cs
+++ CreateNugetCmd/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CreateNugetCmd
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args.Length == 2)
+            {
+                var targetDir = args[0];
+                var csprojPath = args[1];
+
+                var directory = Directory.GetParent(targetDir);
+                var nugetDirectory = directory.Parent.FullName;
+
+                var xml = XDocument.Load(new StreamReader(csprojPath));
+                XElement versionElement = xml.Root.Descendants().Where(n => n.Name == "Version").FirstOrDefault();
+                XElement packageIdElement = xml.Root.Descendants().Where(n => n.Name == "PackageId").FirstOrDefault();
+                if (versionElement == null)
+                {
+                    Console.Error.Write("version element not found");
+                    return -1;
+                }
+                if (packageIdElement == null)
+                {
+                    Console.Error.Write("package id element not found");
+                    return -2;
+                }
+
+                string version = versionElement.Value;
+                string packageId = packageIdElement.Value;
+
+                // Trim last 0 out of version
+                //var parts = version.Split(new char[] { '.'});
+                var real = new Version(version);
+
+                var nugetCmdPath = ConfigurationManager.AppSettings["nugetCmdPath"];
+                if (string.IsNullOrWhiteSpace(nugetCmdPath))
+                {
+                    nugetCmdPath = "nuget";
+                }
+                else if ((nugetCmdPath.IndexOf(' ') >= 0) && (nugetCmdPath.IndexOf('"') == -1))
+                {
+                    nugetCmdPath = string.Concat("\"", nugetCmdPath, "\"");
+                }
+
+                var source = ConfigurationManager.AppSettings["source"];
+                var nupkgName = $"{packageId}.{real}.nupkg";
+
+                //e.g.
+                //nuget add My.Test.Legacy.Models.1.0.1808.12270.nupkg -source \\MyServer\test_packages
+                var cmd = $"{nugetCmdPath} add {nupkgName} -source {source}";
+
+                var path = Path.Combine(nugetDirectory, $"Create-{nupkgName}.cmd");
+
+                File.WriteAllText(path, cmd);
+                return 0;
+            }
+            return -11;
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Create NuGet Cmd
+
+Console program to create a .CMD file that will register a local NuGet package.
